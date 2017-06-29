52845ac42b538f95a43937f3eaba0a707130b04b
Dzulfikar Adi Putra <dzulfikar.adiputra@gmail.com>
2017-06-29 17:00:01 +0900

    * add props to loading-indicator

--- src/components/loading-indicator/LoadingIndicator.vue
+++ src/components/loading-indicator/LoadingIndicator.vue
@@ -5,17 +5,22 @@
 </style>
 
 <template>
-  <div class="loading-wrapper">
+  <div v-if="isLoading" class="loading-wrapper">
     <svg class="progress-circular progress-circular--indeterminate">
       <circle class="progress-circular__background"/>
       <circle class="progress-circular__primary progress-circular--indeterminate__primary"/>
       <circle class="progress-circular__secondary progress-circular--indeterminate__secondary"/>
     </svg>
   </div>
+  <div v-else>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'loading-indicator',
+  props: {
+    isLoading: Boolean,
+  },
 };
 </script>
--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,8 +1,26 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped src="./Navbar.scss">
+<style lang="scss" scoped>
+  .header {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+  img {
+    max-width: 150px;
+  }
+  ons-list-item {
+    cursor: pointer;
+  }
 </style>
-<template src="./Navbar.html">
-  <div></div>
+
+<template>
+  <v-ons-toolbar class="home-toolbar">
+    <div class="left">
+      <v-ons-toolbar-button @click="toggleMenu()">
+        <v-ons-icon icon="ion-navicon, material:md-menu"></v-ons-icon>
+      </v-ons-toolbar-button>
+    </div>
+    <div class="center">{{ msg }}</div>
+  </v-ons-toolbar>
 </template>
 
 <script>
